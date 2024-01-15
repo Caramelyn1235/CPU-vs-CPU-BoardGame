54ff41443d5bdc8fa0197bc94222ef29c06e4d49
Carolyn <carolyn.po.lee68@gmail.com>
2024-01-15 15:03:29 -0500

    git commit -m

--- DVD Collection/src/DVDView.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-public interface DVDView {
-    //Cause view to update its appearance based on given model & selected DVD
-    public void update(DVDCollection model, int selectedDVD);
-
-}
